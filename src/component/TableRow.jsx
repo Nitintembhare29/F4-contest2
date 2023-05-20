612f5d534543d046130612bd40672508c4bec477
Nitin Tembhare <nitintembhare340@gmail.com>
2023-05-20 20:39:22 +0530

    first commit

---
+++ src/component/TableRow.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const TableRow = ({ data }) => {
+  console.log(data);
+  const { name, id, image, symbol, current_price, total_volume, price_change_percentage_24h, market_cap} = data;
+
+  return (
+    <tr>
+      <td>
+        <img src={image} alt={name} width="40" height="40" />
+      </td>
+      <td>{name}</td>
+      <td>{symbol}</td>
+      <td>${current_price}</td>
+      <td>${total_volume}</td>
+      <td style={price_change_percentage_24h < 0 ? { color: "red" } : { color: "green" }}>{price_change_percentage_24h}%</td>
+      <td>${market_cap}</td>
+    </tr>
+  );
+};
+
+export default TableRow;
